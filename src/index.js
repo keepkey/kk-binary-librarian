6c8f9b67bb82ecda6a9f7d3dbdf912a8822f0caf
Ryan <ryan@keepkey.com>
2018-08-20 13:26:01 -0600

    add async/await

--- src/index.js
+++ src/index.js
@@ -1,78 +1,90 @@
-let request = require('request');
+let request = require('request-promise');
 let { sha3_256 } = require('js-sha3');
 let ByteBuffer = require('bytebuffer');
 
 let githubReleaseUrl = 'https://api.github.com/repos/keepkey/keepkey-firmware/releases';
 
-function options(url) {
+function options(url, encoding = undefined) {
   return {
     url: url,
-    encoding: null,
+    encoding: encoding,
     headers: {
       'User-Agent': 'request',
     }
   };
 }
 
-function getFlashAssets() {
-  let assetData = {};
+async function getFlashAssets() {
+  try {
+    // get all tagged binary releases from github
+    const response = await request(options(githubReleaseUrl));
+    const data = JSON.parse(response);
+    const rawPackagedAssets = await findFlashAssets(data);
+    // console.log('final asssets', rawPackagedAssets)
+    console.log('final asssets', JSON.stringify(rawPackagedAssets))
+  }
+  catch (error) {
+    return false;
+  }
+}
 
-  request.get(options(githubReleaseUrl), function(err, _, body) {
-    let res = JSON.parse(body);
+async function findFlashAssets(responseData) {
+  try {
+    let rawData = {};
 
-    res.sort(sortByTagNum).forEach((data) => {
-      data.assets.forEach((asset) => {
+    // pull tag and url from each release to download binary
+    for (const jsonData of responseData) {
+      for (const asset of jsonData.assets) {
+        // only download binary (no tar.bz2 or sig)
         if (asset.name.includes('.bin')) {
+        console.log('Downloading binary from:', jsonData.tag_name, asset.name)
           let url = asset.browser_download_url;
           let padTotal = asset.name.includes('bootstrap') ? 16 : 256;
-          // assetData[data.tag_name] = packageRawAsset(url, data.tag_name, asset.name, padTotal);
-          let rawdata = packageRawAsset(url, data.tag_name, asset.name, padTotal);
+          rawData[jsonData.tag_name] = await packageRawAsset(url, asset.name, padTotal);
         }
-      });
-    });
-  });
-  // return assetData;
+      }
+    }
+
+    return rawData;
+  }
+  catch (error) {
+    console.log('err', error);
+  }
 }
 
-function sortByTagNum(a, b) {
-  return b.tag_name - a.tag_name;
-}
-
-function packageRawAsset(url, tagName, name, padTotal) {
-  let rawAsset = {};
-
-  request.get(options(url), (err, _, body) => {
+async function packageRawAsset(url, name, padTotal) {
+  try {
     let rawData;
+    let rawAsset = {};
     let sectorSize = 1024*padTotal;
-    let size = Math.max(0, (sectorSize - body.length));
+
+    // fetch binary for each tagged release
+    const body = await request(options(url, null));
+
+    // pad out any binary with 0xFFFFFFFF to 256k
     let data = ByteBuffer.wrap(body);
-    console.log('remaining', size);
-    // console.log(url)
-    console.log(tagName)
-    console.log(name)
     if (body.length <= sectorSize) {
       rawData = ByteBuffer.allocate(sectorSize);
       rawData.fill(0xff);
       rawData.reset();
-      data.prependTo(rawData, body.length)
-      console.log('raw buffer length', rawData)
-      console.log('data buffer length', data)
+      data.prependTo(rawData, body.length);
     } else {
-      rawData = ByteBuffer.wrap(body)
+      rawData = ByteBuffer.wrap(body);
     }
-    // let rawData = ByteBuffer.wrap(body)
-    let hash = sha3_256(rawData.toArrayBuffer());
-    // let base64 = rawData.toBase64();
-    // rawAsset.b64_asset = base64
-    // rawAsset.hash = hash
-    console.log('hash', hash);
-    // console.log('b64', base64.slice(0, 100));
-    console.log('=========================');
-  });
-  // console.log(rawAsset)
 
-  return rawAsset;
+    // create hash and base64 string
+    let hash = sha3_256(rawData.toArrayBuffer());
+    // REMOVE SLICE!!
+    let base64 = rawData.toBase64().slice(0, 100);
+    rawAsset[name] = {
+      hash: hash,
+      base64: base64
+    };
+    return rawAsset;
+  }
+  catch (error) {
+    return false;
+  }
 }
 
-
-getFlashAssets();
+module.exports = getFlashAssets();
