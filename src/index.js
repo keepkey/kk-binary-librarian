1408bf6613d4b51e4b868c4211930f10d6bd1f7e
Ryan <ryan@keepkey.com>
2018-08-19 22:14:08 -0600

    WIP: binary hashes of bootloader and firmware are correct, clean up needed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 let request = require('request');
-let {sha3_256} = require('js-sha3');
+let { sha3_256 } = require('js-sha3');
 let ByteBuffer = require('bytebuffer');
 
 let githubReleaseUrl = 'https://api.github.com/repos/keepkey/keepkey-firmware/releases';
@@ -7,44 +7,72 @@ let githubReleaseUrl = 'https://api.github.com/repos/keepkey/keepkey-firmware/re
 function options(url) {
   return {
     url: url,
+    encoding: null,
     headers: {
-      'User-Agent': 'request'
+      'User-Agent': 'request',
     }
-  }
+  };
+}
+
+function getFlashAssets() {
+  let assetData = {};
+
+  request.get(options(githubReleaseUrl), function(err, _, body) {
+    let res = JSON.parse(body);
+
+    res.sort(sortByTagNum).forEach((data) => {
+      data.assets.forEach((asset) => {
+        if (asset.name.includes('.bin')) {
+          let url = asset.browser_download_url;
+          let padTotal = asset.name.includes('bootstrap') ? 16 : 256;
+          // assetData[data.tag_name] = packageRawAsset(url, data.tag_name, asset.name, padTotal);
+          let rawdata = packageRawAsset(url, data.tag_name, asset.name, padTotal);
+        }
+      });
+    });
+  });
+  // return assetData;
 }
 
 function sortByTagNum(a, b) {
   return b.tag_name - a.tag_name;
 }
 
-function packageRawAsset(url, padTotal) {
-  let rawAsset = {}
+function packageRawAsset(url, tagName, name, padTotal) {
+  let rawAsset = {};
 
   request.get(options(url), (err, _, body) => {
+    let rawData;
+    let sectorSize = 1024*padTotal;
+    let size = Math.max(0, (sectorSize - body.length));
     let data = ByteBuffer.wrap(body);
-    console.log("data hex", data.toHex().length);
-    // let padding = ByteBuffer.wrap("0\xff") * (1024*padTotal - data.limit)
+    console.log('remaining', size);
+    // console.log(url)
+    console.log(tagName)
+    console.log(name)
+    if (body.length <= sectorSize) {
+      rawData = ByteBuffer.allocate(sectorSize);
+      rawData.fill(0xff);
+      rawData.reset();
+      data.prependTo(rawData, body.length)
+      console.log('raw buffer length', rawData)
+      console.log('data buffer length', data)
+    } else {
+      rawData = ByteBuffer.wrap(body)
+    }
+    // let rawData = ByteBuffer.wrap(body)
+    let hash = sha3_256(rawData.toArrayBuffer());
+    // let base64 = rawData.toBase64();
+    // rawAsset.b64_asset = base64
+    // rawAsset.hash = hash
+    console.log('hash', hash);
+    // console.log('b64', base64.slice(0, 100));
+    console.log('=========================');
   });
+  // console.log(rawAsset)
 
   return rawAsset;
 }
 
-request.get(options(githubReleaseUrl), function(err, _, body) {
-  let res = JSON.parse(body);
-  let assetData = {};
 
-  res.sort(sortByTagNum).forEach((data) => {
-    assetData[data.tag_name] = {};
-
-    data.assets.forEach((asset) => {
-      console.log("asset name", asset.name);
-      if (asset.name.includes('keepkey_main')) {
-        let url = asset.browser_download_url;
-        let padTotal = asset.name.includes('bootloader') ? 16 : 256;
-        let packagedAsset = packageRawAsset(url, padTotal);
-        // assetData[asset.name] = packageRawAsset(url);
-      }
-    });
-  });
-});
-
+getFlashAssets();
